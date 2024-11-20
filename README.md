05d427c81aecb58256ea7d65b3f21052c1563362
FemBryn <brendenpriddy1@gmail.com>
2024-11-20 11:37:15 -0600

    Ainb Management
    Conversion and reading of Ainb Files as Jsons. working next on the gui.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # What is this?
 
-This is a fork of https://github.com/dt-12345/ainb, aiming to createa visual node editor for ainb files, editing them through use of JSON.
+This is a fork of <https://github.com/dt-12345/ainb>, aiming to createa visual node editor for ainb files, editing them through use of JSON.
--- ainb/Editor.py
+++ ainb/Editor.py
@@ -2,7 +2,25 @@ from converter import json_to_ainb
 from converter import ainb_to_json
 import os
 from pathlib import Path
-
+import json
+##------------------------Converting and Reading Ainb Data------------------------##
+# creating current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 print(current_dir)
-ainb_to_json(Path( str(current_dir) + "\\" +input("  Name of the file to edit? Name, not Path. :: ")))
+
+#save the file name
+filename = input("  Name of the file to edit? (ex: 'LogicTest.root.ainb') :: ")
+
+#convert the ainb to a Json
+ainb_to_json( Path( str(current_dir) + "\\" + filename), str(current_dir) + filename[0:-4])
+
+#Saving the path to the new Json
+JsonPath = current_dir + "\\" + filename[0:-4] + "json"
+filename = filename[0:-4]
+
+# Open the JSON file
+with open(JsonPath, encoding="utf8") as data:
+    # Load the JSON data into a Python dictionary
+    data = json.load(data)
+
+##------------------------Pygame------------------------##
